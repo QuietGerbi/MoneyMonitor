d661bf55e8092c6ca0de4a2d355680565eba327f
QuietGerbi <arhipos497@gmail.com>
2026-04-27 09:07:01 +0700

    first version of full cycle project

--- pipeline.sh
+++ pipeline.sh
@@ -1,25 +1,19 @@
-#!/bin/bash
-set -e  # Остановить выполнение, если какая-то команда упадет
+set -e
 
-# Цвета для красоты
 GREEN='\033[0;32m'
 NC='\033[0m'
 
 echo -e "${GREEN}>>> Starting Full Pipeline Simulation...${NC}"
 
-# STAGE 1: LINT
 echo -e "\n${GREEN} [1/3] Running Stage: LINT${NC}"
 docker run --rm -v $(pwd):/app -w /app python:3.12-slim sh -c "pip install flake8 && flake8 . --exclude=venv,migrations,lib,bin,include"
 
-# STAGE 2: BUILD
 echo -e "\n${GREEN} [2/3] Running Stage: BUILD${NC}"
-# Собираем образ твоего приложения
 docker build -t moneymonitor_app:latest .
 
-# STAGE 3: CLEANUP (Твой кастомный скрипт)
 echo -e "\n${GREEN} [3/3] Running Stage: CLEANUP${NC}"
 chmod +x scripts/cleanup.sh
 ./scripts/cleanup.sh
 
-echo -e "\n${GREEN}>>> PIPELINE PASSED SUCCESSFULLY! 🚀${NC}"
+echo -e "\n${GREEN}>>> PIPELINE PASSED SUCCESSFULLY ${NC}"
 
